51040c9cb8111c5c25b90330058a1ecd34e3f11e
Bhavanapriya12 <bhavanapriyanagella123@gmail.com>
2024-08-16 16:13:50 +0530

    added add project route

--- models/add_projects.js
+++ models/add_projects.js
@@ -10,7 +10,7 @@ const Project_schema=new mongoose.Schema({
     status: { type: String, required: true },
     team: { type: Array, required: true },
     project_status: { type: String, required: true},
-    created_by:{ type: Array, required: true },
+    created_by:{ type: Object, required: true },
     modified_by:{ type: Array, required: true },
 
 
