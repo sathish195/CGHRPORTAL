d0aa8307df81a149ce92e0f1380844b6cde815ca
Bhavanapriya12 <bhavanapriyanagella123@gmail.com>
2024-08-28 15:02:40 +0530

    added birthdays in universal

--- routes/emp_routes/emp_get.js
+++ routes/emp_routes/emp_get.js
@@ -84,6 +84,7 @@ router.post("/get_tasks",Auth, async (req, res)=>{
   // data=req.body;
   // var { error } =validations.get_project_by_id(data);
   //   if (error) return res.status(400).send(error.details[0].message);
+  
 
   const userRole = req.employee.role_name.toLowerCase();
   if (userRole === 'manager') {
