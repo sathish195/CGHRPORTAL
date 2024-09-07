f5012622289889b51827af19f0b8e438afc00094
Bhavanapriya12 <bhavanapriyanagella123@gmail.com>
2024-09-07 14:36:32 +0530

    Added changes in org details

--- routes/org_routes/org.js
+++ routes/org_routes/org.js
@@ -389,7 +389,7 @@ router.post("/add_update_role", Auth, async (req, res) => {
 
 router.post("/universal" ,Auth,async(req, res) => {
     org=await mongoFunctions.find_one("ORGANISATIONS", {
-        email: req.employee.organisation_id,
+        organisation_id: req.employee.organisation_id,
       });
     console.log(org);
     if (!org){
