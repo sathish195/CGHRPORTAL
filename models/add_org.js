d90723126ecaccaeb2babc277c660afe12c82b7b
Bhavanapriya12 <bhavanapriyanagella123@gmail.com>
2024-09-09 14:31:23 +0530

    Added changes in add update leave route

--- models/add_org.js
+++ models/add_org.js
@@ -8,17 +8,19 @@ const ORGANISATION_Schema = new mongoose.Schema(
     employee_id: { type: String, required: true, unique: true }, // Corrected field name to match 'employee_id'
 
     departments: { type: Array, default: [] },
-    roles: { type: Array, default: [] },
-    // designations: {, default: [] },
-    designations: [{
-      designation_id: { type: String, required: true },
-      designation_name: { type: String, required: true },
+    roles: [{
+      role_id: { type: String, required: true },
+      role_name: { type: String, required: true },
+      admin_type:{ type: String, required: true },
       leaves: [{
         leave_id: { type: String, required: true },
         leave_name: { type: String },
         total_leaves: { type: Number }
       }]
     }],
+    // { type: Array, default: [] },
+    // designations: {, default: [] },
+    designations:{ type: Array, default: [] },
 
     organisation_details: { type: Object, default: {} },
     images: { type: Object, default: {} },
