5177a710ac74a754a4acd1ffb0331d477f6832c8
Admin <admin@MPs-MacBook-Air.local>
2025-07-12 19:11:19 +0530

    Given access to emp in add update lead route

--- helpers/schema.js
+++ helpers/schema.js
@@ -70,6 +70,7 @@ function emp_reset_password(data) {
 
 const base64ImageSizeValidator = (value, helpers) => {
   const buffer = Buffer.from(value, "base64");
+  console.log("Decoded size in KB:", buffer.length / 1024);
   const sizeInBytes = buffer.length;
   const limitBytes = 256 * 1024;
   if (sizeInBytes <= limitBytes) return value;
