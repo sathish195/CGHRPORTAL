03e3ac789d8260f2b2b2f118af447c8726182f1b
Admin <admin@MPs-MacBook-Air.local>
2025-07-23 17:12:28 +0530

    changes in all routes to add slowDown

--- models/listings.js
+++ models/listings.js
@@ -1,31 +1,25 @@
 const mongoose = require("mongoose");
 
-const locationSchema = new mongoose.Schema({
-  address: { type: String },
-  city: { type: String },
-  state: { type: String },
-  country: { type: String },
-  pincode: {
-    type: String,
-    match: [/^\d{6}$/, "Pincode must be a 6-digit number"],
-  },
-  landmark: { type: String, default: "" },
-  latitude: { type: Number, default: 0 },
-  longitude: { type: Number, default: 0 },
-});
-
-const amenitySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  count: { type: Number, required: true, min: 0 },
-});
-
-const imageSchema = new mongoose.Schema({
-  url: { type: String, required: true },
-});
-
 const listingSchema = new mongoose.Schema(
   {
-    location: { type: locationSchema, default: {} },
+    location: {
+      type: [
+        {
+          address: { type: String },
+          city: { type: String },
+          state: { type: String },
+          country: { type: String },
+          pincode: {
+            type: String,
+            match: [/^\d{6}$/, "Pincode must be a 6-digit number"],
+          },
+          landmark: { type: String, default: "" },
+          latitude: { type: Number, default: 0 },
+          longitude: { type: Number, default: 0 },
+        },
+      ],
+      default: [],
+    },
     organisation_id: { type: String, required: true },
     key: { type: String, required: true },
     type: { type: String, required: true },
@@ -39,8 +33,23 @@ const listingSchema = new mongoose.Schema(
     description: { type: String, required: true },
     area_sqft: { type: String, required: true },
 
-    amenities: { type: [amenitySchema], default: [] },
-    images: { type: [imageSchema], default: [] },
+    amenities: {
+      type: [
+        {
+          name: { type: String, required: true },
+          count: { type: Number, required: true, min: 0 },
+        },
+      ],
+      default: [],
+    },
+    images: {
+      type: [
+        {
+          url: { type: String, required: true },
+        },
+      ],
+      default: [],
+    },
   },
   { timestamps: true }
 );
