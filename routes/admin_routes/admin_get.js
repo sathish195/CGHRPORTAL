a3f44bea23b77c149196f494c8b6c77bcaac9758
Admin <admin@MPs-MacBook-Air.local>
2025-07-24 15:13:17 +0530

    changes in all routes to add slowDown

--- routes/admin_routes/admin_get.js
+++ routes/admin_routes/admin_get.js
@@ -1196,11 +1196,10 @@ router.post(
     const filters = {
       organisation_id,
     };
-    if (req.employee.admin_type !== "1") {
-      filters.assigned_to = {
-        $elemMatch: { employee_id: req.employee.employee_id },
-      };
+    if (!["1", "2"].includes(req.employee.admin_type)) {
+      filters["assigned_to.employee_id"] = req.employee.employee_id;
     }
+
     // ✅ Optional status filter (case-insensitive)
     if (data.status && data.status !== "") {
       filters.status = { $regex: `^${data.status}$`, $options: "i" };
