11dc0b38e76e515453a18e640c2700a89dfdc3f5
Admin <admin@MPs-MacBook-Air.local>
2025-10-03 15:31:55 +0530

    changes in BITGETSPOTCLIENT

--- routes/org_routes/events.js
+++ routes/org_routes/events.js
@@ -97,7 +97,7 @@ router.post(
   rateLimit(60, 20),
   Async(async (req, res) => {
     const rawInput = encrypt_decrypt.decryptobj(req.body.enc);
-    console.log(rawInput);
+    // console.log(rawInput);
 
     // ✅ Validate input
     const { error, value: data } = validations.add_update_listings(rawInput);
@@ -211,7 +211,7 @@ router.post(
   rateLimit(60, 60),
   Async(async (req, res) => {
     const data = encrypt_decrypt.decryptobj(req.body.enc);
-    console.log(data);
+    // console.log(data);
 
     // Validate limit & skip
     const { error } = validations.get_listings(data);
